375488ef43fe27091b0b1b192f32ae98baf4192d
Jose Maria Zamora <josema.genomics@gmail.com>
2020-12-18 10:31:42 -0600

    Update: fix randsamples.py

--- launch/randsamples.py
+++ launch/randsamples.py
@@ -1,16 +1,26 @@
 import pandas as pd
-import random
+import random, sys
+from pathlib import Path
 
+# Settings
 nsample = 100
-mat = pd.read_csv("Norm_tumorSC.txt", sep = "\t")
+nmats = 10
+
+# Filesystem
+fin = sys.argv[1]
+fout = 'Out/' + fin.split('.')[0]
+Path("Out").mkdir(exist_ok=True)
+
+# Running
+mat = pd.read_csv(sys.argv[1], sep = "\t")
 muestras = mat.shape[1]-1
-for i in range(10):
-	print('Iteracion :' + str(i))
+for i in range(nmats):
+	print('Iteracion ' + str(i))
 	rlist = random.sample(range(1, muestras), nsample)
 	rlist.insert(0,0)
 	newmat = mat.iloc[:,rlist]
-	print(newmat)
-	fname = 'Out/TSC-rand-' + str(i+1) + '.tsv'
+	# print(newmat)
+	fname = fout + '-rand-' + str(i+1) + '.tsv'
 	newmat.to_csv(fname,sep = '\t',index = None)
 
 
